c12210014b75cdf6322a548da867c98f328a842c
bhumikaPandeyCodes <140793724+bhumikaPandeyCodes@users.noreply.github.com>
2025-06-21 13:04:34 +0530

    client-route-added

--- api/index.js
+++ api/index.js
@@ -44,6 +44,7 @@ app.use(express.json());
 import userRoute from "../routes/user/user_routes.js"
 import leadRoute from "../routes/lead/lead_routes.js"
 import contactRoute from "../routes/contact/contact_routes.js"
+import clientRoute from "../routes/client/client_routes.js"
 
 // const userRoute = require("../routes/user/user_routes");
 // const roleRoute = require("../routes/role/role_routes");
@@ -54,6 +55,7 @@ import contactRoute from "../routes/contact/contact_routes.js"
 app.use("/v1/users", userRoute);
 app.use("/v1/leads", leadRoute);
 app.use("/v1/contacts", contactRoute);
+app.use("/v1/clients",clientRoute)
 
 // app.use("/v2/role", roleRoute);
 
