f0e7775989701821cdd99c30ad3cf1066edcade8
bhumikaPandeyCodes <140793724+bhumikaPandeyCodes@users.noreply.github.com>
2025-07-04 11:23:02 +0530

    auth

--- services/auth/auth_services.js
+++ services/auth/auth_services.js
@@ -98,7 +98,7 @@ class LoginService {
     // If we reach this point, authentication was successful for either admin or non-admin.
 
     // Create JWT payload and token
-    const payload = { id: user._id, role: user.role };
+    const payload = { id: user._id, role: user.role, email: user.email, name: user.name };
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRES_IN || '30d',
     });
